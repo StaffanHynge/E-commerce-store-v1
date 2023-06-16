951361e2c0bb23072dc902372bbc19176d33aa10
StaffanHynge <>
2023-06-16 11:45:44 +0000

    add details page

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import AddEvent
+from .views import AddEvent, EventList, EventDetail
 
 urlpatterns = [
     path('', AddEvent.as_view(), name='add_event'),
+    path('eventlist/', EventList.as_view(), name='event_list'),
+    path('<int:pk>/', EventDetail.as_view(), name='event_detail'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Events
 from .forms import EventForm
+
+
+class EventList(ListView):
+    """View all Events"""
+    template_name = 'events/events.html'
+    model = Events
+    context_object_name = 'events'
+
+
+class EventDetail(DetailView):
+    """Allows user to see the detail of the event"""
+    template_name = 'events/event_detail.html'
+    model = Events
+    context_object_name = 'event'
+
 
 
 class AddEvent(LoginRequiredMixin, CreateView):

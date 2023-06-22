0ccab28e4a9158e36ed83d1e0a1bbb540bd46812
StaffanHynge <>
2023-06-22 12:17:01 +0000

    add calendar to date

--- events/forms.py
+++ events/forms.py
@@ -1,25 +1,38 @@
 from django import forms
+from django.forms import DateInput, TimeInput
 from djrichtextfield.widgets import RichTextWidget
-from .models import Events 
+from .models import Events
 
-class EventForm(forms.ModelForm): 
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class TimeInout(forms.TimeInput):
+    input_type = 'time'
+
+
+class EventForm(forms.ModelForm):
     """ A form to create an event"""
 
-    class Meta: 
+    class Meta:
         model = Events
-        fields = ['name', 'description', 'price', 'location', 'date', 'time', 'image']
+        fields = ['name', 'description', 'price',
+                  'location', 'date', 'time', 'image', ]
         description = forms.CharField(widget=RichTextWidget())
 
-        widget = {
+        widgets = {
             "description": forms.Textarea(attrs={"rows": 5}),
+            "date": DateInput(),
+            "time": TimeInput()
         }
 
         labels = {
-            "title" : "Event Title",
-            "description" : "Description",
-            "price" : "Price",
-            "location" : "Location",
-            "date" : "Date",
-            "time" : "Time",
-            "image" : "Image",
+            "title": "Event Title",
+            "description": "Description",
+            "price": "Price",
+            "location": "Location",
+            "date": "Date",
+            "time": "Time",
+            "image": "Image",
         }

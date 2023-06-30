86b64f3cd6cad2d5d90bd481383c2c9ce9817173
StaffanHynge <>
2023-06-30 10:19:56 +0000

    add profile template and render profile image, name and the profiles created events

--- profiles/forms.py
+++ profiles/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from .models import Profile
 
 
-class ProfileForm(forms.ModelForm): 
+class ProfileForm(forms.ModelForm):
     ''' Form to create a profile '''
-    class Meta: 
+    class Meta:
         model = Profile
         fields = ['image', 'about', 'real_name']
 
         labels = {
-            'image' : 'Profile Picture'
-            'real_name' : 'Name'
-            'about' : 'About Me'
+            'image': 'Profile Picture',
+            'real_name': 'Name',
+            'about': 'About Me',
         }

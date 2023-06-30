6f4b33c1851cd41eebc785e433faee6747dfa485
StaffanHynge <>
2023-06-30 09:47:50 +0000

    created form to create profile and edit profile function

---
+++ profiles/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Profile
+
+
+class ProfileForm(forms.ModelForm): 
+    ''' Form to create a profile '''
+    class Meta: 
+        model = Profile
+        fields = ['image', 'about', 'real_name']
+
+        labels = {
+            'image' : 'Profile Picture'
+            'real_name' : 'Name'
+            'about' : 'About Me'
+        }
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from .models import Profile
+from .forms import ProfileForm
 
-# Create your views here.
+
+class Profiles(TemplateView):
+    '''User Profile View'''
+    template_name = 'profiles/profile.html'
+
+    def get_context_data(self, **kwargs):
+        profile = Profile.objects.get(user=self.kwargs['pk'])
+        context = {
+            'profile': profile
+        }
+
+        return context
+
+
+class EditProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    ''' Edit your Profile '''
+    
+    form_class = ProfileForm
+    model = Profile
+
+    def form_valid(self, form): 
+        self.success_url = f'/Profile/view/(self.kwargs["pk"])'
+        return super().form_valid(form)
+
+    def test_func(self): 
+        return self.request.user == self.get_object().user

7276c0e8db62b1695c3ca7659bfb505824ab01e4
StaffanHynge <>
2023-07-21 13:24:55 +0000

    add edit function to venues

--- venues/urls.py
+++ venues/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import AddVenue, VenueList, VenueDetail, DeleteVenue
+from .views import AddVenue, VenueList, VenueDetail, DeleteVenue, EditVenue
 
 
 urlpatterns = [
     path('', AddVenue.as_view(), name='add_venue'),
-    path('venuelist', VenueList.as_view(), name='venue_list'),
+    path('venuelist/', VenueList.as_view(), name='venue_list'),
     path('<int:pk>/', VenueDetail.as_view(), name='venue_detail'),
     path('delete/<int:pk>/', DeleteVenue.as_view(), name='delete_venue'),
+    path('edit/<int:pk>/', EditVenue.as_view(), name='edit_venue'),
 ]
+
 
--- venues/views.py
+++ venues/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, DetailView, DeleteView
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 from .models import Venues
 from .forms import VenueForm
 from django.contrib.auth.mixins import (
@@ -8,15 +8,17 @@ from django.contrib.auth.mixins import (
 
 # Create your views here.
 
-class AddVenue(CreateView): 
+
+class AddVenue(CreateView):
     template_name = 'venues/add_venue.html'
     model = Venues
     form_class = VenueForm
     success_url = '/venues/'
 
-    def form_valid(self, form): 
+    def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddVenue, self).form_valid(form)
+
 
 class VenueList(ListView):
     template_name = 'venues/venues.html'
@@ -24,17 +26,24 @@ class VenueList(ListView):
     context_object_name = 'venues'
 
 
-class VenueDetail(DetailView): 
+class VenueDetail(DetailView):
     template_name = 'venues/venue_detail.html'
     model = Venues
     context_object_name = 'venue'
 
 
+class EditVenue(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = 'venues/edit_venue.html'
+    model = Venues
+    form_class = VenueForm
+    success_url = '/venues/venuelist/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
+
 class DeleteVenue(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Venues
     success_url = '/venues/venuelist/'
 
-    def test_func(self): 
+    def test_func(self):
         return self.request.user == self.get_object().user
-
-
